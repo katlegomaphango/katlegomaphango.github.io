b7faca155ac2458ba20fa975ec379834c2f65db6
Katlego Maphango <mphangokatlego08@gmail.com>
2024-08-06 05:20:01 +0200

    highlight current tab

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.querySelector('.nav-toggle')
 const navMenu = document.querySelector('.nav')
 
+const currentPath = window.location.pathname
+const navLinks = document.querySelectorAll('.nav-link')
+
 toggleBtn.addEventListener('click', () => {
     navMenu.classList.toggle('active')
+})
+
+navLinks.forEach(link => {
+    if (link.getAttribute('href') === `.${currentPath}`) {
+        link.classList.add('active')
+    }
 })
